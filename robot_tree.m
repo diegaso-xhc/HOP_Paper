8584bd373a927d58e325d60c447a78f7d7b96715
diegaso-xhc <52799270+diegaso-xhc@users.noreply.github.com>
2021-01-20 17:13:02 +0100

    First trial for robot tree design

---
+++ robot_tree.m
@@ -0,0 +1,73 @@
+clear all
+close all
+clc
+
+link1 = rigidBody('link1');
+
+jnt1 = rigidBodyJoint('jnt1', 'revolute');
+jnt1.HomePosition = 0;
+tform = trvec2tform([0.0 0.15 0]);
+setFixedTransform(jnt1, tform);
+link1.Joint = jnt1;
+
+link2 = rigidBody('link2');
+jnt2 = rigidBodyJoint('jnt2', 'revolute');
+jnt2.HomePosition = 0;
+tform2 = trvec2tform([0.5 0 0]);
+setFixedTransform(jnt2, tform2);
+link2.Joint = jnt2;
+
+link3 = rigidBody('link3');
+jnt3 = rigidBodyJoint('jnt3', 'revolute');
+jnt3.HomePosition = 0;
+tform3 = trvec2tform([0.5 0 0]);
+setFixedTransform(jnt3, tform3);
+link3.Joint = jnt3;
+
+bodyEndEffector = rigidBody('endeffector');
+tform5 = trvec2tform([0.25, 0, 0]); % User defined
+setFixedTransform(bodyEndEffector.Joint,tform5);
+
+robot = rigidBodyTree;
+robot.DataFormat = 'row'; % To allow users to modify joint values in vector form
+addBody(robot, link1, 'base');
+addBody(robot, link2, 'link1');
+addBody(robot, link3, 'link2');
+addBody(robot,bodyEndEffector,'link3');
+
+figure;
+robot.show
+figure;
+config = randomConfiguration(robot);
+tformf = getTransform(robot,config,'endeffector','base');
+show(robot,config)
+figure;
+config2 = [pi/6, -pi/2, 0]; % These angles need to be limited
+tformf2 = getTransform(robot,config2,'endeffector','base');
+show(robot,config2)
+
+gik = generalizedInverseKinematics('RigidBodyTree',robot,'ConstraintInputs',{'joint', 'pose'});
+jointConst = constraintJointBounds(robot);
+jointConst.Bounds = [-pi/2 pi/6; -pi/2 0; -pi/2 0];
+
+%%%% These lines are for the orientation constraint, but it might already
+%%%% be embedded on the pose target constraint
+% M = (-pi/2)*[0 0 1];
+% quat = quaternion(M, 'rotvec');
+% orientationConst = constraintOrientationTarget(endeffector)
+% orientationConst.TargetOrientation = compact(quat)
+
+poseConst = constraintPoseTarget('endeffector');
+axang = [0 0 1 -pi/4];
+rotm = axang2rotm(axang);
+% M = [0 1 0 1;-1 0 0 -0.2;0 0 1 0;0 0 0 1];
+M = eye(4);
+M(1:3,1:3) = rotm;
+M(1:3,4) = [0.9 -0.3 0]';
+poseConst.TargetTransform = M;
+poseConst.PositionTolerance = 0.004; % 4mm of tolerance from target position
+
+configSol = gik([0.5934 -1.1424 -1.0552],jointConst,poseConst);
+figure;
+show(robot,configSol)
+tformf_solv = getTransform(robot,configSol,'endeffector','base');
